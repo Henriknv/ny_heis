16a6e791b0b060177897d4e887eec02f5cb59680
weedle1912 <weedle1912@gmail.com>
2016-04-17 07:51:18 +0200

    next order test

--- elev/elev.go
+++ elev/elev.go
@@ -257,8 +257,10 @@ func Calculate_next_order(calculate_order_ch <-chan map[string]Elev_info, elev_i
 									if local_cost_this_order < lowest_network_cost{
 										lowest_network_cost = local_cost_this_order
 									}
-
-									local_cost_this_order = local_cost_this_order-1
+									if order_elevator == elev_id{
+										local_cost_this_order = local_cost_this_order-1
+									}
+									
 
 								}
 							}
